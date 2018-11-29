04bdd8726309a97b6e0660538b2b44189d916627
Nancyb <nancyeb27@yahoo.com>
2018-11-28 18:32:15 -0700

    added concertThis

--- liri.js
+++ liri.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
+var axios = require("axios");
 
 var keys = require("./keys.js");
-
-
 
 var liriArgument = process.argv[2];
 var userCommand = process.argv[3];
@@ -14,18 +13,25 @@ switch (liriArgument) {
     case "do-what-it-says": doWhatItSays(); break;
 
 }
-
 // functions
 function movieThis() {
-
+    var movie = process.argv[3];
+    if(!movie) {
+        movie = "mr nobody";
+    }
+        params = movie
+        
+        axios.get("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true&apikey=trilogy").then(
+  function(response) {
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);
 }
 
 function spotifyThisSong() {
     keys.spotifyKeys.search({ type: 'track', query: userCommand }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-
-              
 
         }
         console.log(data.tracks.items[0]);
@@ -35,6 +41,20 @@ function spotifyThisSong() {
 }
 
 function concertThis() {
+    var band = process.argv[4];
+    if(!band) {
+        band = "The Sign";
+    }
+        params = band
+        
+        axios.get("https://rest.bandsintown.com/artists/" + params + "/events?app_id=anythingfortheappidwillwork").then(
+            function (response) {
+    console.log("The name of the artist is: ",response);
+  }
+  
+);
+
+
     console.log("this");
 }
 
